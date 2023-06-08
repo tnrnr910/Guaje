c4f5a457fa8b80d74d4fad7d696b61bb93f01702
saebyeok <nws5412@naver.com>
2023-06-08 10:09:10 +0900

    third commit

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 // DOM 요소
 const searchInput = document.querySelector("#search-input"); // 검색 input 요소
 const cardList = document.querySelector(".card-list"); // 카드 리스트 요소
+const sortBtn = document.getElementById("sort-btn");
+const changeHeader = document.querySelector(".change-header");
+
+// local dom 요소 
+
+const form = document.querySelector(".form-local-data"),
+  localInput = form.querySelector("#form-id"),
+  greeting = document.querySelector(".js-greeting"),
+  button = document.querySelector(".local-delete");
 
 // 영화 검색 input에 포커스
 searchInput.focus();
+
+// valid 유효성검사
+const vaildInput = ['!', '@', '#', '$', '*'];
+
+
+// setInterval change header부분 
+changeHeader.addEventListener('click', () => {
+  setInterval(() => console.log(Math.floor(Math.random() * 10)), 2000);
+  
+});
+
+
+
 
 // 영화 검색 이벤트 핸들러
 const handleSearch = (event) => {
@@ -19,10 +41,13 @@ const handleSearch = (event) => {
     } else {
       card.style.display = "none"; // 일치하는 검색어가 없으면 카드를 숨김
     }
+    movieCards.forEach((card) => {
+      if (searchText.includes(vaildInput))
+        card.style.color = 'red'; // 일치하는 검색어가 있으면 카드를 표시
+    });
+
   });
 };
- 
-//
 
 // 영화 데이터 가져오기
 const fetchMovieData = async () => {
@@ -43,6 +68,7 @@ const fetchMovieData = async () => {
 
 // 영화 카드 생성
 const createMovieCards = async () => {
+
   const movieData = await fetchMovieData(); // 영화 데이터 가져오기
 
   const movieCardsHTML = movieData
@@ -57,8 +83,8 @@ const createMovieCards = async () => {
     `
     )
     .join(""); // 영화 데이터를 반복하여 카드 HTML 문자열 생성
+  setTimeout(() => cardList.innerHTML = movieCardsHTML, 1500);; // 카드 리스트에 생성한 HTML 삽입 1.5초뒤 
 
-  cardList.innerHTML = movieCardsHTML; // 카드 리스트에 생성한 HTML 삽입
 
   const movieCards = document.querySelectorAll(".movie-card"); // 모든 영화 카드 요소 가져오기
   movieCards.forEach((card) => {
@@ -70,6 +96,111 @@ const createMovieCards = async () => {
   });
 };
 
+// 정렬메소드시작
+sortBtn.addEventListener('click', () => {
+  card.sort(function (a, b) {
+    return a - b;
+  });
+});
 // 영화 카드 생성 및 이벤트 핸들러 등록
 createMovieCards(); // 페이지 로드 시 영화 카드 생성
 searchInput.addEventListener("input", handleSearch); // 검색 input에 입력 이벤트 리스너 등록
+
+
+// localst
+
+
+//저장할 데이터의 key값
+const LOCAL_DATA = "nickName";
+
+//input태그에 이름 입력 후 input태그를 감추고,
+//텍스트가 나타나게 해주는 함수
+function hiddenAndGreeting(name, value) {
+  greeting.style.display = "block";
+  form.style.display = "none";
+  button.style.display = "block";
+  button.addEventListener("click", onClick);
+  // const data = new FormData(form);
+  // for (const [name, value] of data) {
+  // console.log(name, ":", value)
+  const data = new FormData(form);
+  for (const [name, value] of data) {
+    // console.log(name, ":", value)
+    // localStorage.setItem(LOCAL_DATA, localInput.value);
+    // hiddenAndGreeting(localInput.value);
+    greeting.innerText = `Hi!${name}${name}${name}`;
+    localInput.value = "";
+  }
+  // }
+}
+
+//버튼 클릭 시, 데이터 삭제 이벤트 발생
+function onClick() {
+  localStorage.removeItem(LOCAL_DATA);
+  console.log("hi");
+  loaded();
+}
+
+//input태그에 입력 시, lacalStorage의 value값으로 저장
+function onSubmit(e) {
+  e.preventDefault();
+  // const data = new FormData(form);   이부분을 위에 선언하면 적용이 안되는데 이유? 
+  const data = new FormData(form);
+  for (const [name, value] of data) {
+    console.log(name, ":", value)
+    localStorage.setItem(LOCAL_DATA, localInput.value);
+    hiddenAndGreeting(localInput.value);
+
+    localInput.value = "";
+  }
+}
+
+//input태그 보이게 설정, 인사 텍스트와 버튼은 숨김.
+//form에 submit 이벤트 설정
+function askForNickName() {
+  form.style.display = "block";
+  greeting.style.display = "none";
+  button.style.display = "none";
+  form.addEventListener("submit", onSubmit); //form에 submit이벤트 추가
+}
+
+// 브라우저의 localStorage에 데이터가 있을때와 없을때
+//구분하여 실행 시켜줄 함수.
+function loaded() {
+  const data = localStorage.getItem(LOCAL_DATA);
+  if (data === null) { // 데이터가 없을 시
+    askForNickName();
+  } else {//데이터가 이미 있을 시
+    hiddenAndGreeting(data);
+  }
+}
+
+//처음 실행할 init함수
+function init() {
+  loaded();
+}
+
+init();
+
+
+
+// 조건부 필터 
+
+function onClick() {
+  localStorage.removeItem(LOCAL_DATA);
+  console.log("hi");
+  loaded();
+}
+
+// $("#sortPrice").click(function () {
+//   products.sort(function (a, b) {
+//     return a.price - b.price;
+//   });
+
+
+
+
+// $("#sortPrice").click(function () {
+//   products.sort(function (a, b) {
+//     return a.price - b.price;
+//   });
